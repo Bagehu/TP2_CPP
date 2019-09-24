051870ecfa8b5d6445339f2334ce950d745f283a
rouillearthur <rouillearthur@protonmail.com>
2019-09-24 17:06:09 +0200

    Oui

--- src/rational.cpp
+++ src/rational.cpp
@@ -12,16 +12,38 @@ public:
   }
 
   Rational(long p, long q) {
-    long g=-1;
+    long pgcd=-1;
     if (q > 0) {
-      while(g != 0) {
-	g = p%q;
-	p = q;
-	b = g;
-      }
-      long sign = (q > 0)? 1 : -1;
-      r = sign * ((p/g) / (abs(q)/g));
+      r = euclide(p,q);
     }
   }
+  long euclide(long p, long q) {
+    long pgcd = -1;
+    long g = 0;
+    while(pgcd != 0) {
+      pgcd = p%q;
+      p = q;
+      q = pgcd;
+    }
+    long sign = (q > 0)? 1 : -1;
+    g = sign * ((p/pgcd) / (abs(q)/pgcd));
+    return g;
+  }
+  
+  inline void getNum() const;
+  
+private:
+  long r;
+};
 
+void Rational::getNum() const {
+  return r;
+}
+ 
+int main() {
+  Rational r1;
+  cout << r1.getNum() << endl;
+
+  //Rational r2(5,3);
+  return 0; 
 }
